193801dbe10d9c266505af0ed5ae770d7b129a47
Matthew Wiser <matt.wiser@icloud.com>
2022-03-05 14:03:31 -0600

    finished initial timer logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+var startButtonEl = document.getElementById("game-start");
+var timerEl = document.getElementById("timer");
+
+function gameStart(){
+    var timer = document.getElementById("timer");
+    timer = parseInt(timer);
+    timerLogic(timer);
+}
+
+function timerLogic(timer){
+    timerEl.innerText =  "5";
+    var currentTime = parseInt(timerEl.innerText);
+    var countdown = setInterval(function(){
+        if (currentTime > 0) {
+            currentTime--;
+            console.log(currentTime);
+            timerEl.innerText = currentTime;
+        } else {
+            clearInterval(countdown);
+            console.log("timer finished");
+            return;
+        }
+    }, 1000)
+}
+
+startButtonEl.addEventListener("click", gameStart);
