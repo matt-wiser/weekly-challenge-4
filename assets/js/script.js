95a962a56cfc64d97c8c0c521df7b85bd17bb8ed
Matthew Wiser <matt.wiser@icloud.com>
2022-04-01 20:47:24 -0500

    further progress on game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,40 @@
 var startButtonEl = document.getElementById("game-start");
 var timerEl = document.getElementById("timer");
 var contentAreaEl = document.getElementById("main-content");
-var questions = {
-    question1 : { questionText : "Commonly used data types do NOT include:", answer1 : "strings", answer2 : "booleans", answer3 : "numbers", answer4 : "alerts", correctAnswer: 4},
-    question2 : { questionText : "The condition in an if/else statement is enclosed in:", answer1 : "quotes", answer2 : "curly brackets", answer3 : "parenthesis", answer4 : "square brackets", correctAnswer: 3},
-    question3 : { questionText : "Arrays in Javascript can be used to store:", answer1 : "numbers and strings", answer2 : "other arrays", answer3 : "booleans", answer4 : "all of the above", correctAnswer: 4}
-};
-var questionContainerEl = document.createElement("div").setAttribute("id", "question-container");
-var questionTextEl = document.createElement("h1").setAttribute("id", "question-text");
+
+var questions = [
+    { questionText : "Commonly used data types do NOT include:", answer1: { answer : "strings", isCorrect : false }, answer2: {answer : "booleans", isCorrect : false } , answer3: { answer : "numbers", isCorrect: false}, answer4: {answer : "alerts", isCorrect: true} }, 
+    { questionText : "The condition in an if/else statement is enclosed in:", answer1: {answer : "quotes", isCorrect : false }, answer2: {answer : "curly brackets", isCorrect : false}, answer3: { answer : "parenthesis", isCorrect : true}, answer4: {answer : "square brackets", isCorrect : false} },
+    { questionText : "Arrays in Javascript can be used to store:", answer1: {answer : "numbers and strings", isCorrect : false}, answer2: { answer : "other arrays", isCorrect : false}, answer3: { answer : "booleans", isCorrect : false }, answer4: { answer : "all of the above", isCorrect : true} }
+];
+var currentTime = 75;
+var questionIterator = 0;
+
+var questionContainerEl = document.createElement("div")
+questionContainerEl.setAttribute("id", "question-container");
+var questionTextEl = document.createElement("h1")
+questionTextEl.setAttribute("id", "question-text");
+
+var answerButton1El = document.createElement("button");
+answerButton1El.setAttribute("type", "button");
+answerButton1El.setAttribute("class", "answer-button");
+
+var answerButton2El = document.createElement("button");
+answerButton2El.setAttribute("type", "button");
+answerButton2El.setAttribute("class", "answer-button");
+
+var answerButton3El = document.createElement("button");
+answerButton3El.setAttribute("type", "button");
+answerButton3El.setAttribute("class", "answer-button");
+
+var answerButton4El = document.createElement("button");
+answerButton4El.setAttribute("type", "button");
+answerButton4El.setAttribute("class", "answer-button");
+
+
 
 function gameStart(){
-    timerEl.innerText =  "75";
-    var currentTime = parseInt(timerEl.innerText);
+    timerEl.innerText =  currentTime;
     var countdown = setInterval(function(){
         if (currentTime > 0) {
             currentTime--;
@@ -33,14 +56,43 @@ function gameEnd(){
 
 function initializeQuestions(){
     contentAreaEl.append(questionContainerEl);
-    questionTextEl.innerText = questions.question1.questionText;
-    questionContainerEl.appendChild(questionTextEl);
+    questionContainerEl.append(questionTextEl);
+    questionContainerEl.append(answerButton1El);
+    questionContainerEl.append(answerButton2El);
+    questionContainerEl.append(answerButton3El);
+    questionContainerEl.append(answerButton4El);
+    
+    var answerButtonsEl = document.querySelectorAll(".answer-button");
+    addAnswerClickListener(answerButtonsEl);
+
+    updateQuestionContent();
+}
+
+function updateQuestionContent(){
+    questionTextEl.innerText = questions[questionIterator].questionText;
+    answerButton1El.innerText = questions[questionIterator].answer1.answer;
+    answerButton2El.innerText = questions[questionIterator].answer2.answer;
+    answerButton3El.innerText = questions[questionIterator].answer3.answer;
+    answerButton4El.innerText = questions[questionIterator].answer4.answer;    
+}
+
+function assessAnswer(clickedElement){
+    console.log("an answer button has been clicked");
+    console.log(clickedElement);
 }
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
+}
+
+function addAnswerClickListener(answerButtonsEl){
+    answerButtonsEl.forEach(element => {
+        element.addEventListener("click", function(event){
+            assessAnswer(event.target);
+        });
+    });
 }
 
 startButtonEl.addEventListener("click", gameStart);
